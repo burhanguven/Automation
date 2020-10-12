8c26f74cd5fdf1f5e81bd07e55f2731d948a720f
burhan.guven <burhanguven34@hotmail.com>
2020-10-13 00:01:56 +0300

    webTableSorting

--- Automation/src/Exercise/JavaStream.java
+++ Automation/src/Exercise/JavaStream.java
@@ -1,6 +1,9 @@
 package Exercise;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.testng.annotations.Test;
@@ -81,6 +84,37 @@ public class JavaStream {
 		names.add("Ali");
 		System.out.println("-----------------");
 		names.stream().filter(s->s.length()>4).limit(2).forEach(s->System.out.println(s));
+	}
+	
+	@Test
+	public void streamMap() {
+			
+		System.out.println("--------streamMap---------");
+		Stream.of("Burhan","Funda","Fenerbahçe","Büþra","Burcu","Ali").filter(s->s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+	}
+	
+	@Test
+	public void streamMapSort() {
+			
+		System.out.println("--------streamMapSort---------");
+		List<String> names=Arrays.asList("Burhan","Funda","Fenerbahçe","Büþra","Burcu","Ali");
+		names.stream().filter(s->s.startsWith("B")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+	}
+	
+	@Test
+	public void streamConcat() {
+		
+		ArrayList<String> names=new ArrayList<String>();
+		names.add("man");
+		names.add("Don");
+		names.add("women");
+		
+		System.out.println("-----------------streamConcat------------------");
+		List<String> names1=Arrays.asList("Burhan","Funda","Fenerbahçe","Büþra","Burcu","Ali");
+		names.stream().filter(s->s.startsWith("B")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+		System.out.println("------");
+		Stream<String> newStream=Stream.concat(names.stream(), names1.stream());
+		newStream.sorted().forEach(s->System.out.println(s));
 	}
 
 }
---
+++ Automation/src/Exercise/WebTableSorting.java
@@ -0,0 +1,50 @@
+package Exercise;
+
+
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class WebTableSorting {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\CAN\\eclipse-workspace\\Automation\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+		
+		//click on column
+		driver.findElement(By.xpath("//tr//th[1]")).click();
+		
+		//capture all webelements into list
+		List<WebElement> elementList=driver.findElements(By.xpath("//tr//td[1]"));
+		
+		//capture text of all webelements into new(original) list
+		List<String> originalList=elementList.stream().map(s->s.getText()).collect(Collectors.toList());
+		
+		//sort on the original list of step 3 ->sorted list
+		List<String> sortedList=originalList.stream().sorted().collect(Collectors.toList());
+		
+		//compare original list vs sorted list
+		Assert.assertTrue(originalList.equals(sortedList));
+		
+		//scan  the name column with getText->rice->print the price og the rice
+		List<String> price=elementList.stream().filter(s->s.getText().contains("Beans")).map(s->getPriceVeggie(s)).collect(Collectors.toList());
+		 price.forEach(a->System.out.println(a));
+	}
+	
+	private static String  getPriceVeggie(WebElement s) {
+		// //tr//td[1]/following-sibling::td[1] price alanýný veriyor
+		String priceValue=s.findElement(By.xpath("following-sibling::td[1]")).getText();
+		return priceValue;
+	}
+
+}
